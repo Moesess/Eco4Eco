b74bcfd92be0c15b3eee3718a14a20e619fabe60
Tomek <tomek@kaszuba.eu>
2023-01-12 15:50:48 +0100

    TK Resource class

--- Assets/Scripts/Resource.cs
+++ Assets/Scripts/Resource.cs
@@ -1,7 +1,103 @@
+using System;
+using UnityEngine;
+
 namespace EcoGameCore
 {
-    public struct Resource {
-        string sName;
-        int iAmount;
+    public class Resource
+    {
+        string sName
+        { // Nazwa surowca
+            get { return this.sName; }
+            set { this.sName = value; }
+        }
+        int iAmount
+        { // Ilosc surowca
+            get { return this.iAmount; }
+            set { this.iAmount = value; }
+        }
+// Konstruktory ============================================================
+        public Resource(int _iVal)
+        {
+            this.sName = "";
+            this.iAmount = _iVal;
+        }
+
+        public Resource(string _sName, int _iVal)
+        {
+            this.sName = _sName;
+            this.iAmount = _iVal;
+        }
+
+// Operatory ===============================================================
+        public static Resource operator +(Resource _Res1, Resource _Res2)
+        {
+            if (_Res1.sName == _Res2.sName)
+                return new Resource(_Res1.sName, _Res1.iAmount + _Res2.iAmount);
+            else
+                throw new Exception("Invalid resource type exception");
+        }
+
+        public static Resource operator +(Resource _Res1, int _iRes2)
+        {
+            return new Resource(_Res1.sName, _Res1.iAmount + _iRes2);
+        }
+
+        public static bool operator <(Resource _Res1, int _iRes2)
+        {
+            return (_Res1.iAmount < _iRes2);
+        }
+
+        public static bool operator >(Resource _Res1, int _iRes2)
+        {
+            return (_Res1.iAmount > _iRes2);
+        }
+
+        public static bool operator ==(Resource _Res1, int _iRes2)
+        {
+            return (_Res1.iAmount == _iRes2);
+        }
+
+        public static bool operator !=(Resource _Res1, int _iRes2)
+        {
+            return (_Res1.iAmount != _iRes2);
+        }
+
+        public static bool operator <(Resource _Res1, Resource _Res2)
+        {
+            return (_Res1.iAmount < _Res2.iAmount);
+        }
+
+        public static bool operator >(Resource _Res1, Resource _Res2)
+        {
+            return (_Res1.iAmount > _Res2.iAmount);
+        }
+
+        public static bool operator ==(Resource _Res1, Resource _Res2)
+        {
+            return (_Res1.iAmount == _Res2.iAmount);
+        }
+
+        public static bool operator !=(Resource _Res1, Resource _Res2)
+        {
+            return (_Res1.iAmount != _Res2.iAmount);
+        }
+
+        public override bool Equals(System.Object _Obj)
+        {
+            if ((_Obj == null) || !(this.GetType().Equals(_Obj.GetType())))
+            {
+                return false;
+            }
+            else
+            {
+                Resource _Res = (Resource)_Obj;
+                return ((this.iAmount == _Res.iAmount) && (this.sName == _Res.sName));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(sName, iAmount);
+        }
     }
 }
--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -5,11 +5,11 @@ namespace EcoGameCore {
     {
         public static ResourceManager Instance;
 
-        private int iPower;  // Energia
-        private int iBudget; // Budzet
-        private int iTrash;  // Smieci
-        private int iFood;   // Zywnosc
-        private int iGoods;  // Towary konsumpcyjne
+        private Resource iPower;  // Energia
+        private Resource iBudget; // Budzet
+        private Resource iTrash;  // Smieci
+        private Resource iFood;   // Zywnosc
+        private Resource iGoods;  // Towary konsumpcyjne
 
         private void Awake()
         {
@@ -18,51 +18,51 @@ namespace EcoGameCore {
                 Instance = this;
         }
 
-        public int GetPower() { return iPower; }
-        public int GetBudget() { return iBudget; }
-        public int GetTrash() { return iTrash; }
-        public int GetFood() { return iFood; }
-        public int GetGoods() { return iGoods; }
+        public Resource GetPower() { return iPower; }
+        public Resource GetBudget() { return iBudget; }
+        public Resource GetTrash() { return iTrash; }
+        public Resource GetFood() { return iFood; }
+        public Resource GetGoods() { return iGoods; }
 
-        public void SetPower(int _iPower) { this.iPower = _iPower; }
-        public void SetBudget(int _iBudget) { this.iBudget = _iBudget; }
-        public void SetTrash(int _iTrash) { this.iTrash = _iTrash; }
-        public void SetFood(int _iFood) { this.iFood = _iFood; }
-        public void SetGoods(int _iGoods) { this.iGoods = _iGoods; }
+        public void SetPower(int _iPower) { this.iPower = new Resource(_iPower); }
+        public void SetBudget(int _iBudget) { this.iBudget = new Resource(_iBudget); }
+        public void SetTrash(int _iTrash) { this.iTrash = new Resource(_iTrash); }
+        public void SetFood(int _iFood) { this.iFood = new Resource(_iFood); }
+        public void SetGoods(int _iGoods) { this.iGoods = new Resource(_iGoods); }
 
         public void AddPower(int _iValue)
         {
             this.iPower += _iValue;
 
-            if (this.iPower < 0) this.iPower = 0;
+            if (this.iPower < 0) this.iPower = new Resource(0);
         }
 
         public void AddBudget(int _iValue)
         {
             this.iBudget += _iValue;
 
-            if (this.iBudget < 0) this.iBudget = 0;
+            if (this.iBudget < 0) this.iBudget = new Resource(0);
         }
 
         public void AddTrash(int _iValue)
         {
             this.iTrash += _iValue;
 
-            if (this.iTrash < 0) this.iTrash = 0;
+            if (this.iTrash < 0) this.iTrash = new Resource(0);
         }
 
         public void AddFood(int _iValue)
         {
             this.iFood += _iValue;
 
-            if (this.iFood < 0) this.iFood = 0;
+            if (this.iFood < 0) this.iFood = new Resource(0);
         }
 
         public void AddGoods(int _iValue)
         {
             this.iGoods += _iValue;
 
-            if (this.iGoods < 0) this.iGoods = 0;
+            if (this.iGoods < 0) this.iGoods = new Resource(0);
         }
     }
 }
